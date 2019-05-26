c6dcbe5549b32b0af5b56518f7fa321df7c6dbca
Blake Rose <bdecktor@gmail.com>
2019-05-26 01:49:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
-  array.each{|element| puts "#{element}.capitalize"}
+  array.each{|element| puts "#{element}.capitalize + "!""}
   array
 end
 
