de3dadc70edb295d4cc6e0c732a4b07ca60e7640
Blake Rose <bdecktor@gmail.com>
2019-05-26 02:05:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find?{|first_cheese| first_cheese[0]}
+  array.find?{|first_cheese, index| first_cheese[0]}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
