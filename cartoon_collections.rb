9e46455f5fc2842e28d2b44429c4251c2d5ab8a0
Blake Rose <bdecktor@gmail.com>
2019-05-26 01:42:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,11 @@
 require 'pry'
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index{ |dwarf, index| puts "#{index+1} #{dwarf}"}
+  dwarves.each_with_index{ |dwarf, index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.each{|element| element}
+  array
 end
 
 def long_planeteer_calls# code an argument here
